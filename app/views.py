30389526c73b28f0b4d8aa852902d687b643670b
Kaor00 <sat.poisen@gmail.com>
2025-10-26 03:25:10 +0300

    Create project

---
+++ app/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserLoginForm
+
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Аккаунт {username} успешно создан")
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, "app/register.html", {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, "Неправильное имя пользователя или пароль!")
+
+    else:
+        form = UserLoginForm()
+    return render(request, 'app/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required
+def home(request):
+    return render(request, 'app/home.html')
+
